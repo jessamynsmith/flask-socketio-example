99713dd295c6ed353b7b68fd4c4698a94a0a6a74
jessamynsmith <geekchick77@gmail.com>
2022-02-09 09:04:07 -0700

    Multiple tries at sending to deepgram on message from front end, with no success.

--- app.py
+++ app.py
@@ -1,25 +1,117 @@
+import asyncio, os, socketio
+
+import flask
 from flask import Flask, render_template
-from flask_socketio import send, SocketIO
+from flask_socketio import emit, send, SocketIO
+from deepgram import Deepgram
+
+
+DEEPGRAM_API_KEY = os.environ.get('DEEPGRAM_API_KEY')
+PATH_TO_FILE = 'you-can-stay-at-home.wav'
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+
+# sio = socketio.AsyncServer()
+# app = socketio.WSGIApp(sio, app)
+
 socket_io = SocketIO(app)
+
+dg_socket = None
+namespace = None
+socket_sid = None
+
+
+def transcript_handler(data):
+    if 'channel' in data:
+        transcript = data['channel']['alternatives'][0]['transcript']
+        print('received transcript:', transcript)
+        send(transcript, namespace='/', to=sid)
+
+
+async def process_audio(connection):
+    print('processing audio')
+    # Open the file
+    with open(PATH_TO_FILE, 'rb') as audio:
+        # Chunk up the audio to send
+        CHUNK_SIZE_BYTES = 8192
+        CHUNK_RATE_SEC = 0.001
+        chunk = audio.read(CHUNK_SIZE_BYTES)
+        while chunk:
+            connection.send(chunk)
+            await asyncio.sleep(CHUNK_RATE_SEC)
+            chunk = audio.read(CHUNK_SIZE_BYTES)
+
+    # Indicate that we've finished sending data
+    print('finishing')
+    await connection.finish()
+    print('finished')
+
+
+# @sio.on('connect')
+# def connect(sid, environ):
+#     print("connected: ", sid)
+# 
+# 
+# @sio.on('message')
+# async def message(sid, data):
+#     print("message ", data)
+#     # await asyncio.sleep(1 * random.random())
+#     # print('waited', data)
+# 
+# 
+# @sio.on('disconnect')
+# def disconnect(sid):
+#     print('disconnect ', sid)
 
 
 @socket_io.on('connect')
 def handle_connection():
-    print('client connected')
+    global namespace, sid
+    namespace = flask.request.namespace
+    sid = flask.request.sid
+    print('client connected', flask.request.namespace, flask.request.sid)
 
 
 @socket_io.on('message')
 def handle_message(data):
-    print('received message: ', data)
+    print('received message: ', data, flask.request.namespace, flask.request.sid)
     send(f"You said: {data}")
+    
+    # loop = asyncio.get_event_loop()
+    # try:
+    #     loop.run_until_complete(process_audio(dg_socket))
+    #     print('complete')
+    # finally:
+    #     loop.close()
 
 
 @app.route("/")
-async def hello_world():
+async def index():
+    global dg_socket, namespace
+
+    # namespace = flask.request.namespace
+    # print('namespace', namespace)
+
+    # Initialize the Deepgram SDK
+    dg_client = Deepgram(DEEPGRAM_API_KEY)
+
+    # Create a websocket connection to Deepgram
+    try:
+        dg_socket = await dg_client.transcription.live({'punctuate': True})
+
+        # Listen for the connection to close
+        dg_socket.registerHandler(dg_socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
+
+        # Print incoming transcription objects
+        dg_socket.registerHandler(dg_socket.event.TRANSCRIPT_RECEIVED, transcript_handler)
+
+        # await process_audio(dg_socket)
+    except Exception as e:
+        print(f'Could not open socket: {e}')
+
+
     return render_template('index.html')
 
 
